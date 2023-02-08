05f2bc334303bebe77c5a656d7d2d90b7081788f
Visa Pollari <visa.pollari@gmail.com>
2023-02-08 15:03:26 +0200

    assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import {
-  TwitterTimelineEmbed,
-  TwitterShareButton,
-  TwitterFollowButton,
-  TwitterHashtagButton,
-  TwitterMentionButton,
-  TwitterTweetEmbed,
-  TwitterMomentShare,
-  TwitterDMButton,
-  TwitterVideoEmbed,
-  TwitterOnAirButton,
-} from 'react-twitter-embed'
+import { Timeline } from 'react-twitter-widgets'
+
 //bg-[#fbbb00]
 const Header = () => {
   return (
     <section className="flex flex-row w-full py-10 h-32 bg-gray-100 ">
       <div className=" w-1/2 items-start">
         <h1 className="text-3xl ml-10 font-bold ">Visa Pollari</h1>
+        <h3 className="text-xl ml-10 font-bold ">Ehdolla uudellamaalla</h3>
       </div>
       <div className="w-1/2 flex mr-10 flex-row justify-end ">
         <div>
@@ -65,36 +56,37 @@ const Home = () => {
   return (
     <main className="flex text-black flex-col items-center bg-white  ">
       <Header />
-      {/**
-      <Image
-        src="/visa-pollari.jpg"
-        className="w-32 h-32 rounded-full m-5"
-        width={88}
-        height={88}
-        alt="Visa Pollari"
-      />
- */}
       <section className="flex flex-col items-center w-full p-10">
         <h2 className="text-2xl font-bold">Miksi olen ehdolla</h2>
-        <p className="text-lg">
-          Olen aktiivisesti seurannut suomen politiikkaa viimeiset 10 vuotta ja
-          olen tietyillä tasoilla turhautunut siihen kuinka politiikkaa tehdään,
-          ja erityisesti kuinka yhteiskunnallista keskustelua käydään.
-          <br />
-          Vaikka en olekaan täysin samaa mieltä kaikista Liberaalipuolueen
-          puolueohjelman tai vaihtoehtobudjetin kohdista, niin itselleni se on
-          vaikuttanut ensimmäiseltä kokonaisvaltaiselta visiolta siitä, mikä
-          valtion rooli on yhteiskunnassa. Valtapuolueet eivät ole kyenneet
-          samalla tavalla tarjoamaan visiota siitä mikä on valtion rooli
-          yhteiskunnassa, vaan monelta osin ottavat nykytilanteen jonain
-          ennalta-annettuna totuutena, ja pyrkivät hiomaan sitä haluamaansa
-          muotoon.
-          <br />
-          <i>
-            Muiden puolueiden etsiessä siis lokaalia maksimia, koen että
-            Liberaalipuolueen tähtäimessä globaali maksimi.
-          </i>
-        </p>
+        <div className="flex flex-row items-center w-full p-10">
+          <Image
+            src="/visa-pollari.jpg"
+            className="w-48 h-48 rounded-full m-5"
+            width={192}
+            height={192}
+            alt="Visa Pollari"
+          />
+          <p className="text-lg">
+            Olen aktiivisesti seurannut suomen politiikkaa viimeiset 10 vuotta
+            ja olen tietyillä tasoilla turhautunut siihen kuinka politiikkaa
+            tehdään, ja erityisesti kuinka yhteiskunnallista keskustelua
+            käydään.
+            <br />
+            Vaikka en olekaan täysin samaa mieltä kaikista Liberaalipuolueen
+            puolueohjelman tai vaihtoehtobudjetin kohdista, niin itselleni se on
+            vaikuttanut ensimmäiseltä kokonaisvaltaiselta visiolta siitä, mikä
+            valtion rooli on yhteiskunnassa. Valtapuolueet eivät ole kyenneet
+            samalla tavalla tarjoamaan visiota siitä mikä on valtion rooli
+            yhteiskunnassa, vaan monelta osin ottavat nykytilanteen jonain
+            ennalta-annettuna totuutena, ja pyrkivät hiomaan sitä haluamaansa
+            muotoon.
+            <br />
+            <i>
+              Muiden puolueiden etsiessä siis lokaalia maksimia, koen että
+              Liberaalipuolueen tähtäimessä globaali maksimi.
+            </i>
+          </p>
+        </div>
       </section>
       <section className="flex flex-col items-center w-full py-10 px-10">
         <h2 className="text-2xl font-bold">Tavoitteeni politiikassa</h2>
@@ -102,7 +94,7 @@ const Home = () => {
           <div className="text-lg mb-3">
             <p>
               <b>
-                Puolueen vaalikanpanjan mukaisesi:{' '}
+                1. Puolueen vaalikanpanjan mukaisesi:{' '}
                 <Link
                   href="https://liberaalipuolue.fi/leikattavaaloytyy"
                   className="text-blue-800"
@@ -121,7 +113,9 @@ const Home = () => {
           </div>
           <div className="text-lg mb-3">
             <p>
-              <b>Sääntelyn keventäminen ja lainsäädännön yksinkertaistaminen</b>
+              <b>
+                2. Sääntelyn keventäminen ja lainsäädännön yksinkertaistaminen
+              </b>
             </p>
             Valtio on liian suuri ja liian monipuolinen toimija, ja se
             näännyttää itse itseään laajenemalla liian usealle sektorille. Lista
@@ -133,7 +127,7 @@ const Home = () => {
           </div>
           <div className="text-lg mb-3">
             <p>
-              <b>Yhteiskunnan rakenne ja poliittinen järjestelmä</b>
+              <b>3. Yhteiskunnan rakenne ja poliittinen järjestelmä</b>
             </p>
             Nykyinen poliittinen järjestelmä edistää vallan keskittymistä
             puolueille. Suomen tulisi poistaa vaalipiirit ja siirtyä
@@ -153,36 +147,27 @@ const Home = () => {
       {/** section with two columns, one for telegram one for twitter */}
       <section className="flex flex-row items-center justify-around w-full py-10 px-10">
         <div className="flex flex-col items-center justify-center w-1/2">
+          <div className="text-lg mb-3">
+            <p>
+              <b>
+                Liity kanavalleni telegramissa:{' '}
+                <Link href="https://t.me/visapollari" className="text-blue-800">
+                  @VisaPollari
+                </Link>
+              </b>
+            </p>
+          </div>
           {tgPosts.map((post) => (
             <div
               className="w-full py-10 px-10"
-              key={post.attributes['data-post'].nodevalue}
+              key={post.innerHTML}
               dangerouslySetInnerHTML={{ __html: post.innerHTML }}
             />
           ))}
         </div>
-        <div className="flex flex-col items-center justify-center w-1/2">
-          <TwitterTimelineEmbed
-            sourceType="profile"
-            screenName="VisaPollari"
-            options={{ height: 400 }}
-          />{' '}
-        </div>
       </section>
       {/** footer */}
-      <footer className="flex flex-row items-center justify-around w-full py-10 bg-black">
-        <a href="https://t.me/visapollari">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 496 512"
-            className="w-10 h-10"
-          >
-            <path
-              fill="white"
-              d="M248,8C111.033,8,0,119.033,0,256S111.033,504,248,504,496,392.967,496,256,384.967,8,248,8ZM362.952,176.66c-3.732,39.215-19.881,134.378-28.1,178.3-3.476,18.584-10.322,24.816-16.948,25.425-14.4,1.326-25.338-9.517-39.287-18.661-21.827-14.308-34.158-23.215-55.346-37.177-24.485-16.135-8.612-25,5.342-39.5,3.652-3.793,67.107-61.51,68.335-66.746.153-.655.3-3.1-1.154-4.384s-3.59-.849-5.135-.5q-3.283.746-104.608,69.142-14.845,10.194-26.894,9.934c-8.855-.191-25.888-5.006-38.551-9.123-15.531-5.048-27.875-7.717-26.8-16.291q.84-6.7,18.45-13.7,108.446-47.248,144.628-62.3c68.872-28.647,83.183-33.623,92.511-33.789,2.052-.034,6.639.474,9.61,2.885a10.452,10.452,0,0,1,3.53,6.716A43.765,43.765,0,0,1,362.952,176.66Z"
-            />
-          </svg>
-        </a>
+      <footer className="grid items-center justify-around w-full py-10 bg-black">
         <a href="https://twitter.com/VisaPollari">
           <svg
             xmlns="http://www.w3.org/2000/svg"
