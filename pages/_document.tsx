8e155ca87594db2614f0c1eb6c19c746c5c26aef
Visa Pollari <visa.pollari@gmail.com>
2023-03-05 02:27:35 +0200

    add ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <script async src="https://platform.twitter.com/widgets.js"></script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9W2F6V6N7L"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-9W2F6V6N7L');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
