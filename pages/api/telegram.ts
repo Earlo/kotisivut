62181b8e1fe7e0321a0124ab5a55b395c43aa948
Visa Pollari <visa.pollari@gmail.com>
2023-11-15 16:37:35 +0200

    cleanup

--- pages/api/telegram.ts
+++ pages/api/telegram.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as cheerio from 'cheerio';
 import Cors from 'cors'
 import type { NextApiRequest, NextApiResponse } from 'next'
@@ -9,14 +8,10 @@ const cors = Cors({
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function runMiddleware(req:NextApiRequest, res: NextApiResponse<string[]>, fn: (req: Cors.CorsRequest, res: {
-  statusCode?: number | undefined;
-  setHeader(key: string, value: string): any;
-  end(): any;
-}, next: (err?: any) => any) => void,
+function runMiddleware(req:NextApiRequest, res: NextApiResponse<string[]>,
 ) {
   return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
+    cors(req, res, (result) => {
       if (result instanceof Error) {
         return reject(result)
       }
@@ -27,7 +22,7 @@ function runMiddleware(req:NextApiRequest, res: NextApiResponse<string[]>, fn: (
 }
 
 const search =  async (req: NextApiRequest, res: NextApiResponse<string[]>) => {
-  await runMiddleware(req, res, cors)
+  await runMiddleware(req, res)
   if (req.method === 'GET') {
     try {
       const response = await fetch(`https://t.me/s/visapollari`)
