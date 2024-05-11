d16ef0cb5d4fa245161ec5a52816e7f6d6d5f461
Visa Pollari <visa.pollari@gmail.com>
2024-05-12 02:50:40 +0300

    text

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import { Telegram } from '@/components/telegram';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => {
   return (
@@ -17,6 +19,46 @@ const Home = () => {
   return (
     <main className="flex flex-col items-center bg-white text-black  ">
       <Header />
+      <section className="flex w-full flex-col items-center p-0 md:p-10">
+        <Image
+          // show when mobile
+          src="/ehdolla.jpg"
+          className="m-5 block h-48 w-48 rounded-full md:hidden"
+          width={192}
+          height={192}
+          alt="Visa Pollari"
+        />
+        <h2 className="text-2xl font-bold">Kuka olen?</h2>
+        <div className="flex w-full flex-col items-center p-4 md:flex-row md:p-10">
+          <Image
+            // hide when mobile
+            src="/ehdolla.jpg"
+            className="m-5 hidden h-48 w-48 rounded-full md:block"
+            width={192}
+            height={192}
+            alt="Visa Pollari"
+          />
+          <p className="text-lg">
+            Visa Pollari, ohjelmistokehittäjä, yrittäjä, ja teekkari Espoosta.
+            <br />
+            Upeat projektit ja hankkeeet joita olen nähnyt ihmisten ympärilläni
+            tekevän ja joita olen päässyt itse tekemään, on luonut uskoa siihen,
+            että ihmisillä voi todella olla omistajuus heitä ympäröivästä
+            maailmasta,
+            <br />
+            Vapaa-aikani kuluu suurelta osin erinäisten projektiluontoisten
+            harrastusten parissa, tapahtumia järjestäen, ohjelmoiden, tai
+            milloin mitäkin. Nautin suuresti filosofisista keskusteluista, sekä
+            leipomisesta. Lenkkeilen säännöllisen epäsäännöllisesti.
+            <br />
+            Haluatko tutustua minuun paremmin? Varaa aika oheisesta linkistä.
+            Mennään vaikka kahville.{' '}
+            <Link href="https://cal.com/visap/30min" className="text-blue-500">
+              Varaa aika
+            </Link>
+          </p>
+        </div>
+      </section>
       <Telegram />
     </main>
   );
