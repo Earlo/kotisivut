e1e2c0b6a124a03e5be84e09b9977e8fcb517d04
Visa P <visa.pollari@gmail.com>
2026-07-08 21:27:37 +0300

    add public key

--- components/generic/footer/Footer.tsx
+++ components/generic/footer/Footer.tsx
@@ -3,7 +3,7 @@ import IconLink from './IconLink';
 
 export default function Footer() {
   return (
-    <footer className="mt-0 flex w-full flex-row items-center justify-around bg-black py-10">
+    <footer className="mt-0 flex w-full flex-row flex-wrap items-center justify-center gap-6 bg-black px-4 py-10">
       <IconLink href="https://twitter.com/VisaPollari" name="Twitter" />
       <IconLink href="https://www.linkedin.com/in/visapollari" name="LinkedIn" />
       <IconLink href="https://github.com/Earlo" name="GitHub" />
@@ -11,6 +11,7 @@ export default function Footer() {
       <IconLink href="https://www.threads.net/@visapollari" name="Threads" />
       <IconLink href="https://suomi.social/@visapollari" name="Mastodon" />
       <IconLink href="https://t.me/visapollari" name="Telegram" />
+      <IconLink href="/public-key.asc" name="PGP" />
     </footer>
   );
 }
