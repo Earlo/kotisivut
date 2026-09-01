3e7d376d4e9704bbf9475139ef2ce0dfd8757f4d
Visa P <visa.pollari@gmail.com>
2026-09-01 13:14:50 +0300

    allow robots

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: ['/', '/blogi/vaalirahoitus'],
+      allow: '/',
     },
     sitemap: `${host}/sitemap.xml`,
     host,
